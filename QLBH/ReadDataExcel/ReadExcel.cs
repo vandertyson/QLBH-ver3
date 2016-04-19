79e4fe1c014d7d73b825271eabd98914e154c645
Đạt đẹp trai <pagontashika14@gmail.com>
2016-04-19 23:24:02 +0700

    dsf

---
+++ QLBH/ReadDataExcel/ReadExcel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToExcel;
+
+namespace ReadDataExcel
+{
+    public class ReadExcel
+    {
+        public static void FromFile<T>(string path)
+        {
+            var excel = new ExcelQueryFactory(path);
+
+        }
+    }
+}
